5cec8d29b25f729c255d9efb524498d0149837d2
Djenifer Muck <129801520+djenimuck@users.noreply.github.com>
2025-04-21 17:44:48 -0300

    corrigido caminho do JSON para GitHub Pages

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = '../data/profile.json';
+    const url = 'https://djenimuck.github.io/portfolio/data/profile.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
